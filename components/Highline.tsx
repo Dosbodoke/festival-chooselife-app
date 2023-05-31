104742d1749d4af924d7fb8acd9a526b88f66c00
Juan Andrade <juangabriel4699@gmail.com>
2023-05-31 08:47:24 -0300

    Remove unnecessary anchor link

--- components/Highline.tsx
+++ components/Highline.tsx
@@ -16,11 +16,9 @@ function Highline({ highline }: Props) {
         <HighlineImage coverImageId={highline.cover_image} />
       </div>
       <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {highline.name}
-          </h5>
-        </a>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {highline.name}
+        </h5>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {highline.description}
         </p>
