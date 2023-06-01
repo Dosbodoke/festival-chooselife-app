087827a8ba7f740b796a11474a31de39c594b3d2
Juan Andrade <juangabriel4699@gmail.com>
2023-06-01 01:00:54 -0300

    Update label

--- components/modal/Pegas.tsx
+++ components/modal/Pegas.tsx
@@ -21,23 +21,23 @@ function Pegas({ radio, time, distance }: Props) {
     <div className="border bg-gray-50 dark:bg-gray-700 dark:border-gray-600 shadow-sm p-4 space-y-3">
       <Radio
         id="entry"
-        label="Entrada"
+        label="Pega"
         value="entry"
-        helperText="Você deu um rolê na fita, especifique a quantidade de metros andados a seguir."
+        helperText="Curtiu um rolêzão na fita"
         registerFunction={radio.registerFunction}
       />
       <Radio
         id="crossing"
         label="Travessia"
         value="crossing"
-        helperText="Você atravessou e voltou com quedas."
+        helperText="Você atravessou e voltou a fita inteira, porém com quedas."
         registerFunction={radio.registerFunction}
       />
       <Radio
         id="cadena"
         label="Cadena"
         value="cadena"
-        helperText="Você dropou no começo e foi até o final da fita sem cair, no final pode descer no drop e sentar na fita."
+        helperText="Você dropou no começo e foi até o final da fita sem cair."
         registerFunction={radio.registerFunction}
       />
       <Radio
@@ -49,7 +49,7 @@ function Pegas({ radio, time, distance }: Props) {
       />
       <TextField
         id="distance"
-        label="Distância"
+        label="Distância caminhada"
         placeholder="Quantos metros você andou"
         inputType="number"
         registerFunction={distance.registerFunction}
