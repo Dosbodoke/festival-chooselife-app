500a8255332f9b4797143014a2c2023ae47dbcdc
Juan Andrade <juangabriel4699@gmail.com>
2023-10-13 11:02:39 -0300

    Added profile page

---
+++ app/[locale]/profile/[username]/_components/UserHeader.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+
+interface Props {
+  username: string;
+}
+
+function UserHeader({ username }: Props) {
+  const t = useTranslations("profile.header");
+
+  return (
+    <header className="flex max-w-screen-md gap-4 rounded-xl border border-gray-200 bg-white px-2 py-4 shadow dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-800">
+      <Image
+        src={"/default-profile-picture.png"}
+        width={128}
+        height={128}
+        alt="Profile picture"
+      />
+      <div className="space-y-3">
+        <h1 className="text-2xl font-semibold">@{username}</h1>
+        <div className="rounded-lg bg-red-50 p-4 text-center text-sm text-red-500 dark:bg-red-100 dark:text-red-700">
+          {t("notVerified")}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default UserHeader;
