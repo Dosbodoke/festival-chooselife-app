29f11cc8d556635f10d79987131202382e3e920a
Juan Andrade <juangabriel4699@gmail.com>
2023-10-21 16:41:38 -0300

    Added new button to log in and log out, adjusted Navbar to fit it

---
+++ app/[locale]/auth/callback/route.ts
@@ -0,0 +1,27 @@
+// Refer to the following documentation for more context
+// https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
+
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { Database } from "@/utils/database.types";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: Request) {
+  // The `/auth/callback` route is required for the server-side auth flow implemented
+  // by the Auth Helpers package. It exchanges an auth code for the user's session.
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+
+  if (code) {
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient<Database>({
+      cookies: () => cookieStore,
+    });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(requestUrl.origin);
+}
