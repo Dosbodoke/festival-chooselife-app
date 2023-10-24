ace4e3f77e6b9c03f4167d787cbc31e2c81518a1
Juan Andrade <juangabriel4699@gmail.com>
2023-10-24 19:13:59 -0300

    SingUp flow with email and google

--- components/ui/Dialog.tsx
+++ components/ui/Dialog.tsx
@@ -75,10 +75,7 @@ const DialogFooter = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
+    className={cn("flex flex-row justify-end space-x-2", className)}
     {...props}
   />
 );
