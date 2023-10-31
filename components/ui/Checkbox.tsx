889f8d5dfebceab62bcd970b6394cbb5d226cace
Juan Andrade <juangabriel4699@gmail.com>
2023-10-31 13:30:43 -0300

    Profile dropdown

--- components/ui/Checkbox.tsx
+++ components/ui/Checkbox.tsx
@@ -1,37 +1,64 @@
-import { UseFormRegisterReturn } from "react-hook-form";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import * as React from "react";
 
-interface Props {
-  id: string;
-  label: string;
-  helperText?: string;
-  registerFunction: UseFormRegisterReturn;
-}
+import { CheckIcon } from "@/assets";
+import { cn } from "@/utils/cn";
 
-function Checkbox({ id, label, helperText, registerFunction }: Props) {
-  return (
-    <div className="flex">
-      <div className="flex h-5 items-center">
-        <input
-          id={id}
-          aria-describedby="helper-checkbox-text"
-          type="checkbox"
-          {...registerFunction}
-          className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
-        />
-      </div>
-      <div className="ml-2 text-sm">
-        <label
-          htmlFor={id}
-          className="font-medium text-gray-900 dark:text-gray-300"
-        >
-          {label}
-        </label>
-        <p className="text-xs font-normal text-gray-500 dark:text-gray-300">
-          {helperText}
-        </p>
-      </div>
-    </div>
-  );
+interface CheckboxProps
+  extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
+  className?: string;
 }
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  CheckboxProps
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "h-4 w-4 rounded border border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 data-[disabled]:cursor-not-allowed  data-[state=checked]:bg-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:data-[state=checked]:bg-blue-500",
+      className
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator className="flex items-center justify-center text-current">
+      <CheckIcon className="text-white" />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export default Checkbox;
+
+interface CheckboxItemProps {
+  id: string;
+  checked: boolean;
+  label: string;
+  value?: string;
+  onCheckedChange: (value: boolean) => void;
+}
+export const CheckboxItem = ({
+  id,
+  checked,
+  label,
+  value,
+  onCheckedChange,
+}: CheckboxItemProps) => {
+  return (
+    <li key={id}>
+      <label
+        htmlFor={`column-${id}`}
+        className="flex items-center gap-2 rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-500"
+      >
+        <Checkbox
+          id={`column-${id}`}
+          checked={checked}
+          onCheckedChange={onCheckedChange}
+          value={value}
+        />
+        <span className="flex-1 rounded text-sm font-medium text-gray-500 dark:text-gray-300">
+          {label}
+        </span>
+      </label>
+    </li>
+  );
+};
