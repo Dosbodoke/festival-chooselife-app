67d08a6a47da4e618e67313f16c5fb17f7da8b00
Juan Andrade <juangabriel4699@gmail.com>
2024-02-02 17:12:06 -0300

    Changed search icon

--- assets/index.tsx
+++ assets/index.tsx
@@ -163,25 +163,6 @@ export const LogOutIcon = (props: SVGAttributes<SVGElement>) => (
   </svg>
 );
 
-export const SearchSvg = (props: SVGAttributes<SVGElement>) => (
-  <svg
-    aria-hidden="true"
-    fill="none"
-    stroke="currentColor"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-5 w-5 text-gray-500 dark:text-gray-400"
-    {...props}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-    ></path>
-  </svg>
-);
-
 export const ArrowLongLeftIcon = (props: SVGAttributes<SVGElement>) => {
   return (
     <svg
