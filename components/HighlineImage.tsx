e99a6c53373dd1bd6b454b99c5056663b2b1ee29
Juan Andrade <juangabriel4699@gmail.com>
2024-02-03 20:27:17 -0300

    Added shadcn Card component and implemnted it on the Highline List

--- components/HighlineImage.tsx
+++ components/HighlineImage.tsx
@@ -1,21 +1,66 @@
+import { cookies } from "next/headers";
 import Image from "next/image";
 
-function HighlineImage({ coverImageId }: { coverImageId: string | null }) {
+import { useSupabaseServer } from "@/utils/supabase/server";
+
+interface HighlinePropsImage {
+  coverImageId: string | null;
+  width?: number;
+  height?: number;
+}
+
+function HighlineImage({ coverImageId, width, height }: HighlinePropsImage) {
+  const cookieStore = cookies();
+  const supabase = useSupabaseServer(cookieStore);
+
+  if (!coverImageId) {
+    return (
+      <Image
+        src={"/chooselife-black.png"}
+        alt="Chooselife Icon"
+        fill
+        priority
+        className="object-contain object-center dark:invert"
+      />
+    );
+  }
+
+  const hasDefinedDimensions = width && height;
+
+  const {
+    data: { publicUrl: URL },
+  } = supabase.storage.from("images").getPublicUrl(
+    `${coverImageId}`,
+    hasDefinedDimensions
+      ? {
+          transform: {
+            width,
+            height,
+          },
+        }
+      : undefined
+  );
+
+  if (hasDefinedDimensions) {
+    return (
+      <Image
+        src={URL}
+        alt="Image of the Highline"
+        width={width}
+        height={height}
+        priority
+        className="object-cover object-center"
+      />
+    );
+  }
+
   return (
     <Image
-      src={
-        coverImageId
-          ? `${
-              process.env.NEXT_PUBLIC_SUPABASE_URL
-            }/storage/v1/object/public/${"images"}/${coverImageId}`
-          : "/chooselife-black.png"
-      }
-      alt="highline"
+      src={URL}
+      alt="Image of the Highline"
       fill
       priority
-      className={`object-center ${
-        coverImageId ? "object-cover" : "object-contain dark:invert"
-      }`}
+      className="object-cover object-center"
     />
   );
 }
