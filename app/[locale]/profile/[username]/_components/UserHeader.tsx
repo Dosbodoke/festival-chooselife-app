1290eef78b04be81f80de4b4b13170b17ffbd3d9
Juan Andrade <78118656+Dosbodoke@users.noreply.github.com>
2024-02-23 11:42:38 -0300

    Added PWA and Heatmap calendar (#30)
    * Added GEIST font
    
    * Ran update-browserslist-db@latest
    
    * Fix import/export order
    
    * Walk calendar (#27)
    
    * Added walk calendar
    
    * Added Walk Activity heatmap calendar
    
    * Replace old cards with the new Card component
    
    * Fix tooltip on mobile
    
    * Fix unverified user card
    
    * PWA (#29)
    
    * Added PWA
    
    * Prompt to install PWA
    
    * Installation tutorial dialog on IOS
    
    * Remove target blank when clicking username

--- app/[locale]/profile/[username]/_components/UserHeader.tsx
+++ app/[locale]/profile/[username]/_components/UserHeader.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
+import { Card, CardContent } from "@/components/ui/card";
 import { Database } from "@/utils/supabase/database.types";
 
 interface Props {
@@ -13,41 +14,47 @@ function UserHeader({ profile, username }: Props) {
 
   if (!profile) {
     return (
-      <header className="flex max-w-screen-md gap-4 rounded-xl border border-gray-200 bg-white px-2 py-4 shadow dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-800">
-        <Image
-          src={"/default-profile-picture.png"}
-          width={128}
-          height={128}
-          alt="Profile picture"
-        />
-        <div className="space-y-3">
-          <h1 className="text-xl font-semibold">@{username}</h1>
-          <div className="rounded-lg bg-red-50 p-4 text-center text-sm text-red-500 dark:bg-red-100 dark:text-red-700">
-            {t("notVerified")}
+      <Card>
+        <CardContent className="flex gap-4 space-y-2 overflow-hidden px-2 py-4">
+          <Image
+            src={"/default-profile-picture.png"}
+            width={128}
+            height={128}
+            alt="Profile picture"
+          />
+          <div className="space-y-3">
+            <h1 className="text-xl font-semibold">@{username}</h1>
+            <div className="rounded-lg bg-red-50 p-2 text-center text-sm text-red-500 dark:bg-red-100 dark:text-red-700 md:p-4">
+              {t("notVerified")}
+            </div>
           </div>
-        </div>
-      </header>
+        </CardContent>
+      </Card>
     );
   }
 
   return (
-    <header className="max-w-screen-md space-y-2 overflow-hidden rounded-xl border border-gray-200 bg-white px-2 py-4 shadow dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-800">
-      <div className="flex gap-4">
-        <div className="relative h-24 w-24 sm:h-32 sm:w-32">
-          <Image
-            src={profile.profile_picture || "/default-profile-picture.png"}
-            fill={true}
-            alt="Profile picture"
-            className="rounded-full"
-          />
+    <Card>
+      <CardContent className="space-y-2 overflow-hidden px-2 py-4">
+        <div className="flex gap-4">
+          <div className="relative h-24 w-24 sm:h-32 sm:w-32">
+            <Image
+              src={profile.profile_picture || "/default-profile-picture.png"}
+              fill={true}
+              alt="Profile picture"
+              className="rounded-full"
+            />
+          </div>
+          <div className="mt-4 flex-1">
+            <h1 className="flex-1 text-xl font-semibold">{profile.name}</h1>
+            <p className="text-muted-foreground">@{username}</p>
+          </div>
         </div>
-        <h1 className="flex-1 text-xl font-semibold">@{username}</h1>
-      </div>
-      <div>
-        <p className="font-medium">{profile.name}</p>
-        <p>{profile.description}</p>
-      </div>
-    </header>
+        <div>
+          <p>{profile.description}</p>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
