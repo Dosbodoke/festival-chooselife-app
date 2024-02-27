bd006fc383c846f9929bcd78d3879e1fbe2fb350
Juan Andrade <juangabriel4699@gmail.com>
2024-02-27 14:09:54 -0300

    Fix HighlineListSkeleton

--- app/[locale]/_components/HighlineListSkeleton.tsx
+++ app/[locale]/_components/HighlineListSkeleton.tsx
@@ -2,10 +2,10 @@ import HighlineImage from "@/components/HighlineImage";
 
 export function HighlineListSkeleton() {
   return (
-    <ul className="flex animate-pulse flex-wrap justify-center gap-6">
+    <>
       {Array.from(Array(8)).map((_, idx) => (
-        <li
-          className="w-full rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800 md:w-96"
+        <div
+          className="flex w-full max-w-[24rem] animate-pulse flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800 md:w-96"
           key={`high-skeleton-${idx}`}
         >
           <div className="relative block h-72 w-full opacity-25">
@@ -16,8 +16,8 @@ export function HighlineListSkeleton() {
             <div className="mt-2 h-3 w-full rounded-lg bg-gray-200 dark:bg-gray-700" />
             <div className="mt-2 h-3 w-56 rounded-lg bg-gray-200 dark:bg-gray-700" />
           </div>
-        </li>
+        </div>
       ))}
-    </ul>
+    </>
   );
 }
