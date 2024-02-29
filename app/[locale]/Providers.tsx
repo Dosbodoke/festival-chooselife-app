b3bc2aca1d36cf28120593be25b77a36a635d7cb
Juan Andrade <juangabriel4699@gmail.com>
2024-02-29 09:34:59 -0300

    Added vercel speed insights

--- app/[locale]/Providers.tsx
+++ app/[locale]/Providers.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
@@ -38,6 +39,7 @@ function Providers({ locale, messages, children }: Props) {
       >
         <ThemeProvider attribute="class" defaultTheme="system">
           <Toaster />
+          <SpeedInsights />
           {children}
         </ThemeProvider>
       </NextIntlClientProvider>
