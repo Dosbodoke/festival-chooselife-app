7f6710df660253b66bcfbe3129828e4261558f26
Juan Andrade <juangabriel4699@gmail.com>
2024-04-02 09:20:37 -0300

    Update React Query setup accordingly to the documentation

--- app/[locale]/Providers.tsx
+++ app/[locale]/Providers.tsx
@@ -6,29 +6,48 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
-import { useState } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+        retry: 2,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
 
 interface Props {
   locale: "en" | "pt";
   messages: AbstractIntlMessages | undefined;
   children: ReactNode;
 }
+
 function Providers({ locale, messages, children }: Props) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 5 * 1000,
-            refetchOnWindowFocus: false,
-            refetchOnReconnect: false,
-            retry: 2,
-          },
-        },
-      })
-  );
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
