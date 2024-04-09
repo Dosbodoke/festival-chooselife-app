331cbf74b3cbaf90f85edcdd62d813935b9353ae
Juan Andrade <juangabriel4699@gmail.com>
2024-04-09 18:00:41 -0300

    Force white theme on map

--- app/[locale]/Providers.tsx
+++ app/[locale]/Providers.tsx
@@ -3,6 +3,7 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { useSearchParams } from "next/navigation";
 import { type AbstractIntlMessages, NextIntlClientProvider } from "next-intl";
 import { ThemeProvider } from "next-themes";
 import type { ReactNode } from "react";
@@ -17,8 +18,12 @@ interface Props {
 }
 
 function Providers({ locale, messages, children }: Props) {
+  const searchParams = useSearchParams();
   const queryClient = getQueryClient();
   const now = new Date();
+
+  const forcedThemeFromSearchParams =
+    searchParams.get("view") === "map" ? "light" : undefined;
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -28,7 +33,11 @@ function Providers({ locale, messages, children }: Props) {
         now={now}
         timeZone="America/Sao_Paulo"
       >
-        <ThemeProvider attribute="class" defaultTheme="system">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          forcedTheme={forcedThemeFromSearchParams}
+        >
           <Toaster />
           <SpeedInsights />
           {children}
